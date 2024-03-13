75e8677c24ef884e016c81e03a2fc080e8fb5ee1
Danil Bohutskiy <bohutskiy2@gmail.com>
2024-03-13 16:37:18 +0200

    Add events

--- classes/App.py
+++ classes/App.py
@@ -1,10 +1,13 @@
 import customtkinter
 from classes.formatters.mappings.FormatterMapping import FormatterMapping
+from classes.factories.FormatterFactory import FormatterFactory
 
 class App(customtkinter.CTk):
 
-    SETTINGS_APP_TITLE = 'AI App'
+    SETTINGS_APP_TITLE = 'Charachter Formatting Types Creator'
     SETTINGS_GEOMETRY_RESOLUTION = '800x600'
+
+    DEFAULT_CHARACHTER_NAME = 'Unknown'
 
     def __init__(self):
         super().__init__()
@@ -36,21 +39,21 @@ class App(customtkinter.CTk):
         self._render_main_label_to_textarea_output()
         self._render_main_texarea_output()
 
-    def _render_main_label_to_textarea_input(self, text = "Input text"):
+    def _render_main_label_to_textarea_input(self, text = "Charachter description"):
         self.main_frame_label_input_textarea = customtkinter.CTkLabel(self.main_frame, text=text)
         self.main_frame_label_input_textarea.pack(side='top')
 
     def _render_main_texarea_input(self):
-        self.main_textbox_input = customtkinter.CTkTextbox(master=self.main_frame, width=800)
-        self.main_textbox_input.pack(side="top")
+        self.main_frame_textbox_input = customtkinter.CTkTextbox(master=self.main_frame, width=800)
+        self.main_frame_textbox_input.pack(side="top")
 
-    def _render_main_label_to_textarea_output(self, text = "Output text"):
+    def _render_main_label_to_textarea_output(self, text = "Generated text"):
         self.main_frame_label_output_textarea = customtkinter.CTkLabel(self.main_frame, text=text)
         self.main_frame_label_output_textarea.pack(side='top')
 
     def _render_main_texarea_output(self):
-        self.main_textbox_output = customtkinter.CTkTextbox(master=self.main_frame, height=500, width=800)
-        self.main_textbox_output.pack(side="bottom")
+        self.main_frame_textbox_output = customtkinter.CTkTextbox(master=self.main_frame, height=500, width=800)
+        self.main_frame_textbox_output.pack(side="bottom")
 
     def _render_frame_main(self):
         self.main_frame = customtkinter.CTkFrame(self, width=600, height=800)
@@ -80,7 +83,7 @@ class App(customtkinter.CTk):
         self.sidebar_frame.pack(side="left", fill="y", expand=True)
 
     def _render_sidebar_generate_button(self, text = "Generate"):
-        self.sidebar_button = customtkinter.CTkButton(self.sidebar_frame, text=text)
+        self.sidebar_button = customtkinter.CTkButton(self.sidebar_frame, text=text, command=self.button_generate_output)
         self.sidebar_button.pack(side="bottom", anchor="sw", pady=10, padx=20)
 
     def _render_sidebar_label_to_dropdown_type(self, text = "Select style text format"):
@@ -100,3 +103,24 @@ class App(customtkinter.CTk):
         self.sidebar_input_charachter_name.pack(side='top')
 
     # ============================= Sidebar - End =============================================#
+        
+    # ============================= Events ====================================================#
+
+    def button_generate_output(self):
+        charachter_name = self.sidebar_input_charachter_name.get()
+        if (charachter_name == ''):
+            charachter_name = self.DEFAULT_CHARACHTER_NAME
+        charachter_description = self.main_frame_textbox_input.get(0.0, 'end')
+        format_type = self.sidebar_dropdown_format.get()
+
+        formatter = FormatterFactory.create(format_type)
+        formatted_text = formatter.format(charachter_name, charachter_description)
+        self._fill_output(formatted_text)
+
+    def _clear_output(self):
+        self.main_frame_textbox_output.delete(0.0, 'end')
+
+    def _fill_output(self, output_text):
+        self._clear_output()
+        self.main_frame_textbox_output.insert('end', output_text)
+        pass
