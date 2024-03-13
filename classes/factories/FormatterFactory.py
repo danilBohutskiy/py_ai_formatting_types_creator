6ecf622d89fa8724ed58ca099a51c6eabc0093c1
Danil Bohutskiy <bohutskiy2@gmail.com>
2024-03-13 14:30:01 +0200

    Upd base & factory classes, add format for WppFormatter

--- classes/factories/FormatterFactory.py
+++ classes/factories/FormatterFactory.py
@@ -1,9 +1,9 @@
 from ..formatters.mappings.FormatterMapping import FormatterMapping
 
 class FormatterFactory:
-    def create(format_type, text):
+    def create(format_type):
         formatter_class = FormatterMapping.FORMATTER_MAPPING.get(format_type)
         if formatter_class:
-            return formatter_class(text)
+            return formatter_class()
         else:
             raise ValueError("Unknown format type!")
--- classes/formatters/WppFormatter.py
+++ classes/formatters/WppFormatter.py
@@ -1,6 +1,13 @@
 from .base.BaseFormatter import BaseFormatter
 
 class WppFormatter(BaseFormatter):
-    @staticmethod
-    def format(self):
-        pass
+    def format(self, text):
+        processed_lines = [self._process_line(line) for line in text.splitlines() if line.strip()]
+        formatter_text = '[{' + '\n'.join(processed_lines) + '}]'
+        return formatter_text
+
+    def _process_line(self, line):
+        key, value = line.split(':')
+        values_joined = ', '.join([f'"{v.strip()}"' for v in value.split(',')])
+        processed_line = f"{key}({values_joined})"
+        return processed_line
