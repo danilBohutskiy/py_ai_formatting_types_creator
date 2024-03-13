0328abcf0fd44cc590930ea673a96b72ed2839f0
Danil Bohutskiy <bohutskiy2@gmail.com>
2024-03-13 15:18:07 +0200

    Add format for BoostyleFormatter

--- classes/formatters/BoostyleFormatter.py
+++ classes/formatters/BoostyleFormatter.py
@@ -1,5 +1,14 @@
 from .base.BaseFormatter import BaseFormatter
 
 class BoostyleFormatter(BaseFormatter):
-    def format(self):
-        pass
+    def format(self, charachter_name, charachter_description_text):
+        processed_lines = [self._process_line(line) for line in charachter_description_text.splitlines() if line.strip()]
+        processed_lines = '+'.join(processed_lines)
+        formatter_text = f'{charachter_name}[{processed_lines}]'
+        return formatter_text
+
+    def _process_line(self, line):
+        key, value = line.split(':')
+        values_joined = '+'.join([f'"{v.strip()}"' for v in value.split(',')])
+        processed_line = f"{values_joined}"
+        return processed_line
